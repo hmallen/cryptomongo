9abb44dd98223161577e068355969725ef6fa9a1
hmallen <allenhm@gmail.com>
2020-03-31 05:15:41 -0600

    Trade stream with save to MongoDB testing successfully.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mongotrades
+
 Dump trade data from Cryptowat.ch websockets into MongoDB via PyMongo. Rough and quickly assembled for use with another project.
+
+## Notes
+
+* Testing with 5 medium to high volume markets didn't show any issues with buffering when saving to db
+* If overload occurs, can easily implement Apache Kafka as stream buffering mechanism
---
+++ mongotrades.py
@@ -0,0 +1,86 @@
+import asyncio
+import configparser
+import functools
+import json
+import logging
+from pprint import pprint
+import signal
+
+from pymongo import MongoClient
+
+import websockets
+from websockets import WebSocketClientProtocol
+
+config_path = 'config/settings.conf'
+config = configparser.ConfigParser()
+config.read(config_path)
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+## Standard Functions ##
+
+def build_subscription(market_list):
+    markets = [
+        {"streamSubscription": {"resource": f"markets:{mkt.strip(' ')}:trades"}} \
+        for mkt in market_list.split(',')]
+    
+    constructed_message = {'subscribe': {'subscriptions': markets}}
+
+    return constructed_message
+
+
+def signal_exit(signame, loop):
+    print("Got signal %s: exit" % signame)
+    loop.stop()
+
+
+#def log_message(message: str) -> None:
+#    logging.info(f"Message: {message}")
+
+
+#def save_mongodb(trade_collection: pymongo.collection.Collection, message: str) -> None:
+#    pass
+
+
+## AsyncIO Functions ##
+
+async def consumer_handler(websocket: WebSocketClientProtocol) -> None:
+    mongo_client = MongoClient(
+        f"mongodb://{config['mongodb']['host']}:{config['mongodb']['port']}/")
+    trade_collection = mongo_client[
+        config['mongodb']['database']][config['mongodb']['collection']]
+    
+    async for message in websocket:
+        # log_message(message)
+
+        message_json = json.loads(message)
+        # pprint(message_json)
+
+        # save_mongodb(trade_collection, message_json)
+        doc_id = trade_collection.insert_one(message_json).inserted_id
+        logger.debug('doc_id: {}'.format(doc_id))
+
+
+async def consume(api_key: str, sub_msg: str) -> None:
+    websocket_resource_url = f'wss://stream.cryptowat.ch/connect?apikey={api_key}'
+    async with websockets.connect(websocket_resource_url) as websocket:
+        await websocket.send(json.dumps(sub_msg))
+        await consumer_handler(websocket)
+
+
+if __name__ == '__main__':
+    subscription_message = build_subscription(config['cryptowatch']['market_ids'])
+    
+    loop = asyncio.get_event_loop()
+    for signame in {'SIGINT', 'SIGTERM'}:
+        loop.add_signal_handler(
+            getattr(signal, signame),
+            functools.partial(signal_exit, signame, loop))
+    
+    try:
+        loop.run_until_complete(
+            consume(api_key=config['cryptowatch']['api'], sub_msg=subscription_message))
+    finally:
+        loop.close()
